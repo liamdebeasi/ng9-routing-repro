20508a5a350c9ced6d5c81671e53204beea1da7a
Liam DeBeasi <liamdebeasi@icloud.com>
2020-02-10 15:11:29 -0500

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'child',
+    loadChildren: () => import('./child/child.module').then( m => m.ChildPageModule)
+  },
 ];
 
 @NgModule({
